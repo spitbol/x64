ee33c4875c71c56e5bfdf40a99031b4e87917543
daveshields <david.shields@yahoo.com>
2012-09-04 23:06:37 -0400

    Update trace code

---
+++ os/atlin.c
@@ -0,0 +1,45 @@
+/*
+Copyright 1987-2012 Robert B. K. Dewar and Mark Emmer.
+
+This file is part of Macro SPITBOL.
+
+    Macro SPITBOL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Macro SPITBOL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Macro SPITBOL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+        zysdc - check system expiration date
+
+        zysdc prints any header messages and may check
+        the date to see if execution is allowed to proceed.
+
+        Parameters:
+            Nothing
+        Returns
+            Nothing
+            No return if execution not permitted
+
+*/
+
+#include "port.h"
+#include "os.h"
+#include "globals.ext"
+
+#include <stdio.h>
+
+extern int nlines;
+atlin()
+{
+   printf(" in atline\n");
+    printf( "atlin %d\n",nlines);
+}
