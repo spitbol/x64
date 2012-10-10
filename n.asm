de7a57641f82f7d56de4a638132681f899f80398
daveshields <david.shields@yahoo.com>
2012-10-09 23:22:00 -0400

    Convert osint sys.. interface routine calls from gas to nasm.

--- n.asm
+++ n.asm
@@ -168,8 +168,8 @@ nlines:	dd   	0
 ;;; These locations save information needed to return after calling OSINT
 ;;; and after a restart from EXIT()
 ;;;
-;;	global	reg_pc
-;;	global	reg_pp
+	extern	reg_pc
+	extern	reg_pp
 ;;reg_pc:        dd   ,0  ; Return PC from ccaller
 ;;reg_pp: dd      0               ; Number of bytes of PPMs
 ;;	global	reg_xs
@@ -330,7 +330,6 @@ re3:	cld
 	cmp	eax,4
         je      re4               	; yes, do a complete fudge
 
-	extern	cc1
 ;
 ;       Jump back to cc1 with return value = NORMAL_RETURN
 	mov	eax,-1
@@ -1063,5 +1062,363 @@ startup:
 ;        MINIMAL START                   # load regs, switch stack, start compiler
         push START
         callc minimal,4
+
+;
+;-----------
+;
+;       Interface routines
+;
+;       Each interface routine takes the following form:
+;
+;               SYSXX   call    ccaller         # call common interface
+;                       dd      zysxx           # address of C OSINT function
+;                       db      n               # offset to instruction after
+;                                               #   last procedure exit
+;
+;       In an effort to achieve portability of C OSINT functions, we
+;       do not take take advantage of any "internal" to "external"
+;       transformation of names by C compilers.  So, a C OSINT function
+;       representing sysxx is named _zysxx.  This renaming should satisfy
+;       all C compilers.
+;
+;       IMPORTANT  ONE interface routine, SYSFC, is passed arguments on
+;       the stack.  These items are removed from the stack before calling
+;       ccaller, as they are not needed by this implementation.
+;
+;
+;-----------
+;
+;       CCALLER is called by the OS interface routines to call the
+;       real C OS interface function.
+;
+;       General calling sequence is
+;
+;               call    ccaller
+;               dd      address_of_C_function
+;               db      2*number_of_exit_points
+;
+;       Control IS NEVER returned to a interface routine.  Instead, control
+;       is returned to the compiler (THE caller of the interface routine).
+;
+;       The C function that is called MUST ALWAYS return an integer
+;       indicating the procedure exit to take or that a normal return
+;       is to be performed.
+;
+;               C function      Interpretation
+;               return value
+;               ------------    -------------------------------------------
+;                    <0         Do normal return to instruction past
+;                               last procedure exit (distance passed
+;                               in by dummy routine and saved on stack)
+;                     0         Take procedure exit 1
+;                     4         Take procedure exit 2
+;                     8         Take procedure exit 3
+;                    ...        ...
+;
+        proc   ccaller,near
+
+;       (1) Save registers in global variables
+;
+        mov     dword [reg_wa],ecx              ; save registers
+	mov	dword [reg_wb],ebx
+        mov     dword [reg_wc],edx              ; (also _reg_ia)
+	mov	dword [reg_xr],edi
+	mov	dword [reg_xl],esi
+        mov     dword [reg_cp],ebp              ; Needed in image saved by sysxi
+
+;       (2) Get pointer to arg list
+;
+        pop     esi                     ; point to arg list
+;
+;       (3) Fetch address of C function, fetch offset to 1st instruction
+;           past last procedure exit, and call C function.
+;
+        cs                              ; CS segment override
+        lodsd                           ; point to C function entry point
+;       lodsd   cs:ccaller              # point to C function entry point
+        movzx   ebx,byte [esi]   ; save normal exit adjustment
+;
+        mov     dword [reg_pp],ebx              ; in memory
+        pop     dword [reg_pc]                  ; save return PC past "CALL SYSXX"
+;
+;       (3a) Save compiler stack and switch to OSINT stack
+;
+        mov     dword [compsp],esp              ; 1.39 save compiler's stack pointer
+        mov     esp,dword [osisp]               ; 1.39 load OSINT's stack pointer
+;
+;       (3b) Make call to OSINT
+;
+        call    eax                     ; call C interface function
+;
+;       (4) Restore registers after C function returns.
+;
+cc1:    mov     dword [osisp],esp               ; 1.39 save OSINT's stack pointer
+        mov     esp,dword [compsp]              ; 1.39 restore compiler's stack pointer
+        mov     ecx, dword [reg_wa]              ; restore registers
+	mov	ebx, dword [reg_wb]
+        mov     edx, dword [reg_wc ]             ; (also reg_ia)
+	mov	edi, dword [reg_xr]
+	mov	esi, dword [reg_xl]
+	mov	ebp, dword [reg_cp]
+
+	cld
+;
+;       (5) Based on returned value from C function (in EAX) either do a normal
+;           return or take a procedure exit.
+;
+        or      eax,eax         ; test if normal return ...
+        jns     erexit    ; j. if >= 0 (take numbered exit)
+	mov	eax,dword [reg_pc]
+        add     eax,dword [reg_pp]      ; point to instruction following exits
+        jmp     eax             ; bypass PPM exits
+
+;                               # else (take procedure exit n)
+erexit: shr     eax,1           ; divide by 2
+        add     eax,dword [reg_pc]      ;   get to address of exit offset
+	movsx	eax,word [eax]
+        add     eax,dword [ppoff]       ; bias to fit in 16-bit word
+	push	eax
+        xor     eax,eax         ; in case branch to error cascade
+        ret                     ;   take procedure exit via PPM address
+
+
+;       Individual OSINT routine entry points
+
+        global SYSAX
+	extern	zysax
+SYSAX:	call	ccaller
+        dd        zysax
+        db     0
+;
+        global SYSBS
+	extern	zysbs
+SYSBS:	call	ccaller
+        dd        zysbs
+        db     3*2
+;
+        global SYSBX
+	extern	zysbx
+SYSBX:	mov	dword [reg_xs],esp
+	call	ccaller
+        dd      zysbx
+        db     0
+;
+;%if SETREAL = 1
+;        global SYSCR
+;	extern	zyscr
+;SYSCR:  call    ccaller
+;        dd      zyscr
+;        db     0
+;;
+;%endif
+        global SYSDC
+	extern	zysdc
+SYSDC:	call	ccaller
+        dd      zysdc
+        db     0
+;
+        global SYSDM
+	extern	zysdm
+SYSDM:	call	ccaller
+        dd      zysdm
+        db     0
+;
+        global SYSDT
+	extern	zysdt
+SYSDT:	call	ccaller
+        dd      zysdt
+        db     0
+;
+        global SYSEA
+	extern	zysea
+SYSEA:	call	ccaller
+        dd      zysea
+        db     1*2
+;
+        global SYSEF
+	extern	zysef
+SYSEF:	call	ccaller
+        dd      zysef
+        db     3*2
+;
+        global SYSEJ
+	extern	zysej
+SYSEJ:	call	ccaller
+        dd      zysej
+        db     0
+;
+        global SYSEM
+	extern	zysem
+SYSEM:	call	ccaller
+        dd      zysem
+        db     0
+;
+        global SYSEN
+	extern	zysen
+SYSEN:	call	ccaller
+        dd      zysen
+        db     3*2
+;
+        global SYSEP
+	extern	zysep
+SYSEP:	call	ccaller
+        dd      zysep
+        db     0
+;
+        global SYSEX
+	extern	zysex
+SYSEX:	mov	dword [reg_xs],esp
+	call	ccaller
+        dd      zysex
+        db     3*2
+;
+        global SYSFC
+	extern	zysfc
+SYSFC:  pop     eax             ; <<<<remove stacked SCBLK>>>>
+	lea	esp,[esp+edx*4]
+	push	eax
+	call	ccaller
+        dd      zysfc
+        db     2*2
+;
+        global SYSGC
+	extern	zysgc
+SYSGC:	call	ccaller
+        dd      zysgc
+        db     0
+;
+        global SYSHS
+	extern	zyshs
+SYSHS:	mov	dword [reg_xs],esp
+	call	ccaller
+        dd      zyshs
+        db     8*2
+;
+        global SYSID
+	extern	zysid
+SYSID:	call	ccaller
+        dd      zysid
+        db     0
+;
+        global SYSIF
+	extern	zysif
+SYSIF:	call	ccaller
+        dd      zysif
+        db     1*2
+;
+        global SYSIL
+	extern	zysil
+SYSIL:  call    ccaller
+        dd      zysil
+        db     0
+;
+        global SYSIN
+	extern	zysin
+SYSIN:	call	ccaller
+        dd      zysin
+        db     3*2
+;
+        global SYSIO
+	extern	zysio
+SYSIO:	call	ccaller
+        dd      zysio
+        db     2*2
+;
+        global SYSLD
+	extern	zysld
+SYSLD:  call    ccaller
+        dd      zysld
+        db     3*2
+;
+        global SYSMM
+	extern	zysmm
+SYSMM:	call	ccaller
+        dd      zysmm
+        db     0
+;
+        global SYSMX
+	extern	zysmx
+SYSMX:	call	ccaller
+        dd      zysmx
+        db     0
+;
+        global SYSOU
+	extern	zysou
+SYSOU:	call	ccaller
+        dd      zysou
+        db     2*2
+;
+        global SYSPI
+	extern	zyspi
+SYSPI:	call	ccaller
+        dd      zyspi
+        db     1*2
+;
+        global SYSPL
+	extern	zyspl
+SYSPL:	call	ccaller
+        dd      zyspl
+        db     3*2
+;
+        global SYSPP
+	extern	zyspp
+SYSPP:	call	ccaller
+        dd      zyspp
+        db     0
+;
+        global SYSPR
+	extern	zyspr
+SYSPR:	call	ccaller
+        dd      zyspr
+        db     1*2
+;
+        global SYSRD
+	extern	zysrd
+SYSRD:	call	ccaller
+        dd      zysrd
+        db     1*2
+;
+        global SYSRI
+	extern	zysri
+SYSRI:	call	ccaller
+        dd      zysri
+        db     1*2
+;
+        global SYSRW
+	extern	zysrw
+SYSRW:	call	ccaller
+        dd      zysrw
+        db     3*2
+;
+        global SYSST
+	extern	zysst
+SYSST:	call	ccaller
+        dd      zysst
+        db     5*2
+;
+        global SYSTM
+	extern	zystm
+SYSTM:	call	ccaller
+systm_p: dd      zystm
+        db     0
+;
+        global SYSTT
+	extern	zystt
+SYSTT:	call	ccaller
+        dd      zystt
+        db     0
+;
+        global SYSUL
+	extern	zysul
+SYSUL:	call	ccaller
+        dd      zysul
+        db     0
+;
+        global SYSXI
+	extern	zysxi
+SYSXI:	mov	dword [reg_xs],esp
+	call	ccaller
+sysxi_p: dd      zysxi
+        db     2*2
 
 
