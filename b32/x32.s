2461566d202b10a2c799fc76bdb87bdf07ae3301
daveshields <thedaveshields@gmail.com>
2012-12-10 18:17:24 -0500

    Change license from 'gpl v3 or later' to 'gpl v2 or later'

--- b32/x32.s
+++ b32/x32.s
@@ -1,3 +1,21 @@
+; Copyright 1987-2012 Robert B. K. Dewar and Mark Emmer.
+; Copyright 2012 David Shields
+; 
+; This file is part of Macro SPITBOL.
+; 
+;     Macro SPITBOL is free software: you can redistribute it and/or modify
+;     it under the terms of the GNU General Public License as published by
+;     the Free Software Foundation, either version 2 of the License, or
+;     (at your option) any later version.
+; 
+;     Macro SPITBOL is distributed in the hope that it will be useful,
+;     but WITHOUT ANY WARRANTY; without even the implied warranty of
+;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;     GNU General Public License for more details.
+; 
+;     You should have received a copy of the GNU General Public License
+;     along with Macro SPITBOL.  If not, see <http://www.gnu.org/licenses/>.
+;
 	global	reg_block
 	global	reg_wa
 	global	reg_wb
@@ -16,23 +34,6 @@
 	extern	calltab
 	extern	stacksiz
  
-; Copyright 1987-2012 Robert B. K. Dewar and Mark Emmer.
-; 
-; This file is part of Macro SPITBOL.
-; 
-;     Macro SPITBOL is free software: you can redistribute it and/or modify
-;     it under the terms of the GNU General Public License as published by
-;     the Free Software Foundation, either version 3 of the License, or
-;     (at your option) any later version.
-; 
-;     Macro SPITBOL is distributed in the hope that it will be useful,
-;     but WITHOUT ANY WARRANTY; without even the implied warranty of
-;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;     GNU General Public License for more details.
-; 
-;     You should have received a copy of the GNU General Public License
-;     along with Macro SPITBOL.  If not, see <http://www.gnu.org/licenses/>.
-;
 %define globals 1                       ;ASM globals defined here
 
 ;
