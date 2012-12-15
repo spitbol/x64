7a8e0bdc7a2269cb1ebb22023ebfa7d5aa9e0fe0
daveshields <thedaveshields@gmail.com>
2012-12-15 17:35:50 -0500

    Register assignment for x64

--- x32.s
+++ x32.s
@@ -139,8 +139,7 @@
 ; ; Words saved during exit(-3)
 ; ;
         align 4
-	global	MINCP
-MINCP:	dd	0	; minimal code pointer (CP)
+	extern	MINCP
 reg_block:
 reg_wa:	dd	0        ; Register WA (ECX)
 reg_wb:	dd 	0        ; Register WB (EBX)
@@ -517,8 +516,8 @@ cprtmsg:
 call_adr:	dd	0
 	segment	.text
 
-ccaller:
-;       (1) Save registers in global variables
+syscall_ent:
+;       save registers in global variables
 
         mov     dword [reg_wa],ecx              ; save registers
 	mov	dword [reg_wb],ebx
@@ -527,41 +526,21 @@ ccaller:
 	mov	dword [reg_xl],esi
         mov     dword [reg_cp],ebp              ; Needed in image saved by sysxi
 
-;       (2) Get pointer to arg list
+;       get pointer to arg list
 
-        pop     esi                     ; point to arg list
+        pop     eax                     ; pop return address
+	mov	dword [reg_pc],eax
 
-;       (3) Fetch dd      of C function], fetch offset to 1st instruction
-;           past last procedure exit], and call C function.
 
-        cs                              ; CS segment override
-        lodsd                           ; point to C function entry point
-	mov	dword [call_adr],eax
-;       lodsd   cs:ccaller              ; point to C function entry point
-;	word after callee address used to be ppm count. Now used for debug
-	mov	ebx,dword[esi]
-        mov     dword [reg_pp],ebx              ; in memory
-        pop     dword [reg_pc]                  ; save return PC past "CALL SYSXX"
 
-;       (3a) Save compiler stack and switch to OSINT stack
+	ret
 
-; DS 12/22/12 Note that needn't save and restore stack ptrs if not using
-; 	save files or load modules
-         mov     dword [compsp],esp              ; 1.39 save compiler's stack pointer
-         mov     esp,dword [osisp]               ; 1.39 load OSINT's stack pointer
 
-;       (3b) Make call to OSINT
-	mov	eax,dword [call_adr]
-        call    eax                     ; call C interface function
 
+syscall_exi:
 	mov	dword [_rc_],eax		; save return code from function
-
-;       (4) Restore registers after C function returns.
-
-
-cc1:    
- 	mov     dword [osisp],esp               ; 1.39 save OSINT's stack pointer
-        mov     esp,dword [compsp]              ; 1.39 restore compiler's stack pointer
+ 	mov     dword [osisp],esp               ; save OSINT's stack pointer
+        mov     esp,dword [compsp]              ; restore compiler's stack pointer
         mov     ecx,dword [reg_wa]              ; restore registers
 	mov	ebx,dword [reg_wb]
         mov     edx,dword [reg_wc]              ; (also reg_ia)
@@ -573,246 +552,179 @@ cc1:
 
 	mov	eax,dword [reg_pc]
 	jmp	eax
+	
+	%macro	syscall	2
+	call	syscall_ent
 
-
-;---------------
-
-;       Individual OSINT routine entry points
+;       save compiler stack and switch to OSINT stack
+        mov     dword [compsp],esp              ; save compiler's stack pointer
+        mov     esp,dword [osisp]               ; load OSINT's stack pointer
+	call	%1
+	call	syscall_exi
+	%endmacro
 
         global SYSAX
 	extern	zysax
-SYSAX:	call	ccaller
-        dd        zysax
-        dd   1
+SYSAX:	syscall	  zysax,1
 
         global SYSBS
 	extern	zysbs
-SYSBS:	call	ccaller
-        dd        zysbs
-        dd   2
+SYSBS:	syscall	  zysbs,2
 
         global SYSBX
 	extern	zysbx
 SYSBX:	mov	dword [reg_xs],esp
-	call	ccaller
-        dd      zysbx
-        dd   2
+	syscall	zysbx,2
 
 ;        global SYSCR
 ;	extern	zyscr
-;SYSCR:  call    ccaller
-;        dd      zyscr
-;        dd   0
+;SYSCR:  syscall    zyscr ;    ,0
 ;
         global SYSDC
 	extern	zysdc
-SYSDC:	call	ccaller
-        dd      zysdc
-        dd   4
+SYSDC:	syscall	zysdc,4
 
         global SYSDM
 	extern	zysdm
-SYSDM:	call	ccaller
-        dd      zysdm
-        dd   5
+SYSDM:	syscall	zysdm,5
 
         global SYSDT
 	extern	zysdt
-SYSDT:	call	ccaller
-        dd      zysdt
-        dd   6
+SYSDT:	syscall	zysdt,6
 
         global SYSEA
 	extern	zysea
-SYSEA:	call	ccaller
-        dd      zysea
-        dd   7
+SYSEA:	syscall	zysea,7
 
         global SYSEF
 	extern	zysef
-SYSEF:	call	ccaller
-        dd      zysef
-        dd   8
+SYSEF:	syscall	zysef,8
 
         global SYSEJ
 	extern	zysej
-SYSEJ:	call	ccaller
-        dd      zysej
-        dd   9
+SYSEJ:	syscall	zysej,9
 
         global SYSEM
 	extern	zysem
-SYSEM:	call	ccaller
-        dd      zysem
-        dd   10
+SYSEM:	syscall	zysem,10
 
         global SYSEN
 	extern	zysen
-SYSEN:	call	ccaller
-        dd      zysen
-        dd   11
+SYSEN:	syscall	zysen,11
 
         global SYSEP
 	extern	zysep
-SYSEP:	call	ccaller
-        dd      zysep
-        dd  	12
+SYSEP:	syscall	zysep,12
 
         global SYSEX
 	extern	zysex
 SYSEX:	mov	dword [reg_xs],esp
-	call	ccaller
-        dd      zysex
-        dd   13
+	syscall	zysex,13
 
         global SYSFC
 	extern	zysfc
 SYSFC:  pop     eax             ; <<<<remove stacked SCBLK>>>>
 	lea	esp,[esp+edx*4]
 	push	eax
-	call	ccaller
-        dd      zysfc
-        dd   14
+	syscall	zysfc,14
 
         global SYSGC
 	extern	zysgc
-SYSGC:	call	ccaller
-        dd      zysgc
-        dd   15
+SYSGC:	syscall	zysgc,15
 
         global SYSHS
 	extern	zyshs
 SYSHS:	mov	dword [reg_xs],esp
-	call	ccaller
-        dd      zyshs
-        dd   16
+	syscall	zyshs,16
 
         global SYSID
 	extern	zysid
-SYSID:	call	ccaller
-        dd      zysid
-        dd   17
+SYSID:	syscall	zysid,17
 
         global SYSIF
 	extern	zysif
-SYSIF:	call	ccaller
-        dd      zysif
-        dd   18
+SYSIF:	syscall	zysif,18
 
         global SYSIL
 	extern	zysil
-SYSIL:  call    ccaller
-        dd      zysil
-        dd   19
+SYSIL:  call    	zysil,19
 
         global SYSIN
 	extern	zysin
-SYSIN:	call	ccaller
-        dd      zysin
-        dd   20
+SYSIN:	syscall	zysin,20
 
         global SYSIO
 	extern	zysio
-SYSIO:	call	ccaller
-        dd      zysio
-        dd   21
+SYSIO:	syscall	zysio,21
 
         global SYSLD
 	extern	zysld
-SYSLD:  call    ccaller
-        dd      zysld
-        dd   22
+SYSLD:  call    	zysld,22
 
         global SYSMM
 	extern	zysmm
-SYSMM:	call	ccaller
-        dd      zysmm
-        dd   23
+SYSMM:	syscall	zysmm,23
 
         global SYSMX
 	extern	zysmx
-SYSMX:	call	ccaller
-        dd      zysmx
-        dd   24
+SYSMX:	syscall	zysmx,24
 
         global SYSOU
 	extern	zysou
-SYSOU:	call	ccaller
-        dd      zysou
-        dd   25
+SYSOU:	syscall	zysou,25
 
         global SYSPI
 	extern	zyspi
-SYSPI:	call	ccaller
-        dd      zyspi
-        dd   26
+SYSPI:	syscall	zyspi,26
 
         global SYSPL
 	extern	zyspl
-SYSPL:	call	ccaller
-        dd      zyspl
-        dd   27
+SYSPL:	syscall	zyspl,27
 
         global SYSPP
 	extern	zyspp
-SYSPP:	call	ccaller
-        dd      zyspp
-        dd   28
+SYSPP:	syscall	zyspp,28
 
         global SYSPR
 	extern	zyspr
-SYSPR:	call	ccaller
-        dd      zyspr
-        dd   29
+SYSPR:	syscall	zyspr,29
 
         global SYSRD
 	extern	zysrd
-SYSRD:	call	ccaller
-        dd      zysrd
-        dd   30
+SYSRD:	syscall	zysrd,30
 
         global SYSRI
 	extern	zysri
-SYSRI:	call	ccaller
-        dd      zysri
-        dd   32
+SYSRI:	syscall	zysri,32
 
         global SYSRW
 	extern	zysrw
-SYSRW:	call	ccaller
-        dd      zysrw
-        dd   33
+SYSRW:	syscall	zysrw,33
 
         global SYSST
 	extern	zysst
-SYSST:	call	ccaller
-        dd      zysst
-        dd   34
+SYSST:	syscall	zysst,34
 
         global SYSTM
 	extern	zystm
-SYSTM:	call	ccaller
-systm_p: dd      zystm
-        dd   35
+SYSTM:	syscall	zystm,35
 
         global SYSTT
 	extern	zystt
-SYSTT:	call	ccaller
-        dd      zystt
-        dd   36
+SYSTT:	syscall	zystt,36
 
         global SYSUL
 	extern	zysul
-SYSUL:	call	ccaller
-        dd      zysul
-        dd   37
+SYSUL:	syscall	zysul,37
 
         global SYSXI
 	extern	zysxi
 SYSXI:	mov	dword [reg_xs],esp
-	call	ccaller
-sysxi_p: dd      zysxi
-        dd   38
+	syscall	zysxi,38
+;---------------
+
+;       Individual OSINT routine entry points
+
 ; SPITBOL math routine interface 
 
 	%macro	callext	2
