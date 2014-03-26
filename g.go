793af2bb65c7dd32aa520cec993121263ace897c
daveshields <thedaveshields@gmail.com>
2014-03-25 23:51:54 -0400

    now able to compile s.int, so begin work on g.go, minimal interpreter in go

---
+++ g.go
@@ -0,0 +1,234 @@
+package minimal
+
+func fun() {
+	i := 1
+	src := 1
+	dst := 2
+	off := 3
+	switch i {
+	case mov:
+		m.Reg[dst] = m.Reg[src]
+	case brn:
+		m.IP = dst
+	case bsw:
+
+	case ent:
+	case bri:
+	case lei:
+	case ppm:
+	case prc:
+	case exi:
+	case enp:
+	case err:
+	case sss:
+	case erb:
+	case icv:
+	case dcv:
+	case ssl:
+	case rtn:
+	case add:
+	case sub:
+	case beq:
+	case bne:
+	case ble:
+	case blo:
+	case bhi:
+	case bnz:
+	case bze:
+	case lct:
+	case bct:
+	case aov:
+	case bev:
+	case bod:
+	case lcp:
+	case scp:
+	case lcw:
+	case icp:
+	case ldi:
+	case adi:
+	case mli:
+	case sbi:
+	case dvi:
+	case rmi:
+	case ngi:
+	case ino:
+	case iov:
+	case ieq:
+	case ige:
+	case igt:
+	case ile:
+	case ilt:
+	case ine:
+	case ldr:
+	case mlr:
+	case dvr:
+	case rov:
+	case rno:
+	case ngr:
+	case req:
+	case rge:
+	case rgt:
+	case rle:
+	case rlt:
+	case rne:
+	case plc:
+	case psc:
+	case cne:
+	case cmc:
+	case trc:
+	case flc:
+	case anb:
+	case orb:
+	case xob:
+	case rsh:
+	case lsh:
+	case nzb:
+	case zrb:
+	case mfi:
+	case itr:
+	case rti:
+	case ctb:
+	case ctw:
+	case cvm:
+	case mvc:
+	case mvw:
+	case mwb:
+	case chk:
+
+	case adr:
+	case bge:
+	case bgt:
+	case blt:
+	case call:
+	case callos:
+	case cvd:
+	case decv:
+	case incv:
+	case jsrerr:
+	case load:
+	case loadcfp:
+	case loadi:
+	case move:
+	case nop:
+	case pop:
+	case popr:
+	case push:
+	case pushi:
+	case pushr:
+	case realop:
+	case sbr:
+	case store:
+	}
+}
+
+const (
+	aaa = iota
+	add
+	adi
+	adr
+	anb
+	aov
+	bct
+	beq
+	bev
+	bge
+	bgt
+	bhi
+	ble
+	blo
+	blt
+	bne
+	bnz
+	bod
+	bri
+	brn
+	bsw
+	bze
+	call
+	callos
+	chk
+	cmc
+	cne
+	ctb
+	ctw
+	cvd
+	cvm
+	dcv
+	decv
+	dvi
+	dvr
+	enp
+	ent
+	erb
+	err
+	exi
+	flc
+	icp
+	icv
+	ieq
+	ige
+	igt
+	ile
+	ilt
+	incv
+	ine
+	ino
+	iov
+	itr
+	jsrerr
+	lcp
+	lct
+	lcw
+	ldi
+	ldr
+	lei
+	load
+	loadcfp
+	loadi
+	lsh
+	mfi
+	mli
+	mlr
+	mov
+	move
+	mvc
+	mvw
+	mwb
+	ngi
+	ngr
+	nop
+	nzb
+	orb
+	plc
+	pop
+	popr
+	ppm
+	prc
+	psc
+	push
+	pushi
+	pushr
+	realop
+	req
+	rge
+	rgt
+	rle
+	rlt
+	rmi
+	rne
+	rno
+	rov
+	rsh
+	rti
+	rtn
+	sbi
+	sbr
+	scp
+	ssl
+	sss
+	store
+	sub
+	trc
+	xob
+	zrb
+)
