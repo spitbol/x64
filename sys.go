1f7b05b6889fe4e199f7375dfc09db3725920e24
daveshields <thedaveshields@gmail.com>
2014-04-01 16:55:19 -0400

    more work on go interpreter, add skeleton code for osint procedures in go.

---
+++ sys.go
@@ -0,0 +1,154 @@
+package minimal
+
+func sysax () {
+	panic("sysax not implemented.")
+}
+
+func sysbs () {
+	panic("sysbs not implemented.")
+}
+
+func sysbx () {
+	panic("sysbx not implemented.")
+}
+
+func syscm () {
+	panic("syscm not implemented.")
+}
+
+func sysdc () {
+	panic("sysdc not implemented.")
+}
+
+func sysdm () {
+	panic("sysdm not implemented.")
+}
+
+func sysdt () {
+	panic("sysdt not implemented.")
+}
+
+func sysea () {
+	panic("sysea not implemented.")
+}
+
+func sysef () {
+	panic("sysef not implemented.")
+}
+
+func sysej () {
+	panic("sysej not implemented.")
+}
+
+func sysem () {
+	panic("sysem not implemented.")
+}
+
+func sysen () {
+	panic("sysen not implemented.")
+}
+
+func sysep () {
+	panic("sysep not implemented.")
+}
+
+func sysex () {
+	panic("sysex not implemented.")
+}
+
+func sysfc () {
+	panic("sysfc not implemented.")
+}
+
+func sysgc () {
+	panic("sysgc not implemented.")
+}
+
+func syshs () {
+	panic("syshs not implemented.")
+}
+
+func sysid () {
+	panic("sysid not implemented.")
+}
+
+func sysif () {
+	panic("sysif not implemented.")
+}
+
+func sysil () {
+	panic("sysil not implemented.")
+}
+
+func sysin () {
+	panic("sysin not implemented.")
+}
+
+func sysio () {
+	panic("sysio not implemented.")
+}
+
+func sysld () {
+	panic("sysld not implemented.")
+}
+
+func sysmm () {
+	panic("sysmm not implemented.")
+}
+
+func sysmx () {
+	panic("sysmx not implemented.")
+}
+
+func sysou () {
+	panic("sysou not implemented.")
+}
+
+func syspi () {
+	panic("syspi not implemented.")
+}
+
+func syspl () {
+	panic("syspl not implemented.")
+}
+
+func syspp () {
+	panic("syspp not implemented.")
+}
+
+func syspr () {
+	panic("syspr not implemented.")
+}
+
+func sysrd () {
+	panic("sysrd not implemented.")
+}
+
+func sysri () {
+	panic("sysri not implemented.")
+}
+
+func sysrw () {
+	panic("sysrw not implemented.")
+}
+
+func sysst () {
+	panic("sysst not implemented.")
+}
+
+func systm () {
+	panic("systm not implemented.")
+}
+
+func systt () {
+	panic("systt not implemented.")
+}
+
+func sysul () {
+	panic("sysul not implemented.")
+}
+
+func sysxi () {
+	panic("sysxi not implemented.")
+}
+
